c269c74294d977ac94738c7d7f47d51684e95d7b
KupavaN <pavelkuri@yandex.ru>
2022-04-30 11:57:32 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ДЗ-1
+            string name = "player";
+            byte healf = 150;
+            short armor = 400;
+            int gold = 100000;
+            float jamInLiterJar = 0.8f;
+            string npcHello = "Hello adventurer";
+            bool isArmorBroken = armor <= 0;
+            bool isPlayerAlive = healf > 0;
+            int crystal = 50;
+            double inventoryWeight = 45.0;
+        }
+    }
+}
