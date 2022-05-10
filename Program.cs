f7a15874217b8868b3a0d033635f468c9c51c6cb
KupavaN <pavelkuri@yandex.ru>
2022-05-10 23:56:21 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,28 +9,12 @@ namespace ConsoleApp1
     internal class Program
     {
         static void Main(string[] args)
-        {
-            // ДЗ-5   
-            //string name = "Kurilov";
-            //string surname = "Pavel";
-            //string itog = name + " " + surname;
-            //Console.WriteLine(itog);
-            //string nameCorrect = "Pavel";
-            //string surnameCorrect = "Kurilov";
-            //name = nameCorrect;
-            //surname = surnameCorrect;
-            //string itogNew = name + " " + surname;
-            //Console.WriteLine(itogNew);
-
+        {           
             string name = "Kurilov";
             string surname = "Pavel";
-            string checkName = name;
             string itog = name + " " + surname;
             Console.WriteLine(itog);
-            name = surname;
-            surname = checkName;
-            string itogNew = name + " " + surname;
-            Console.WriteLine(itogNew);
+            
         }
     }
 }
