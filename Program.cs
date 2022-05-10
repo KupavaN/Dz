bebdae4f7193a4a2ac986cb8e3e528ceb6a4b282
KupavaN <pavelkuri@yandex.ru>
2022-05-10 16:11:51 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // ДЗ-5           
+            // ДЗ-5   
+            //string name = "Kurilov";
+            //string surname = "Pavel";
+            //string itog = name + " " + surname;
+            //Console.WriteLine(itog);
+            //string nameCorrect = "Pavel";
+            //string surnameCorrect = "Kurilov";
+            //name = nameCorrect;
+            //surname = surnameCorrect;
+            //string itogNew = name + " " + surname;
+            //Console.WriteLine(itogNew);
+
             string name = "Kurilov";
             string surname = "Pavel";
             string checkName = name;
