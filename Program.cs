ec7121f250060b87c6d962b181a9088693f9b0c7
KupavaN <pavelkuri@yandex.ru>
2022-05-11 14:41:14 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,15 +12,15 @@ namespace ConsoleApp1
         {
             int peopleInLine;
             int appointmentTime = 10;
-            int time;
-            int minutes;
-            int hours;
+            int timeInLine;
+            int minutesInLine;
+            int hoursInLine;
             Console.Write("How many people are in line? ");
             peopleInLine = Convert.ToInt32(Console.ReadLine());
-            time = peopleInLine * appointmentTime;
-            hours = time / 60;
-            minutes = time % 60;
-            Console.WriteLine($"You have to wait for your turn for {hours} hours and {minutes} minutes");
+            timeInLine = peopleInLine * appointmentTime;
+            hoursInLine = timeInLine / 60;
+            minutesInLine = timeInLine % 60;
+            Console.WriteLine($"You have to wait for your turn for {hoursInLine} hours and {minutesInLine} minutes");
         }
     }
 }
