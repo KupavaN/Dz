f91464ca36041b679b9d7a9d234d43b0a9d445d3
KupavaN <pavelkuri@yandex.ru>
2022-05-11 13:38:21 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,17 +9,16 @@ namespace ConsoleApp1
     internal class Program
     {
         static void Main(string[] args)
-        {                      
-            string name = "Kurilov";
-            string surname = "Pavel";          
-            string itog = name + " " + surname;
-            Console.WriteLine(itog);
-            string nameCorrect = "Pavel";
-            string surnameCorrect = "Kurilov";
-            name = nameCorrect;
-            surname = surnameCorrect;
-            string itogNew = name + " " + surname;
-            Console.WriteLine(itogNew);
+        {
+            int coins;
+            int crystalCount;           
+            int crystalPrice = 10;
+            Console.Write("How many coins you have? ");
+            coins = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Price for 1 crystal = 10 coins. How many crystals do you want to buy? ");
+            crystalCount = Convert.ToInt32(Console.ReadLine());
+            coins -= crystalPrice * crystalCount;
+            Console.WriteLine($"You have {crystalCount} crystals and {coins} coins");
         }
     }
 }
