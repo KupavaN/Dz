6bb98686860689335e653e5ce4e0dbf6100f0232
KupavaN <pavelkuri@yandex.ru>
2022-05-11 14:31:59 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,15 +10,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int coins;
-            int crystalsCount;           
-            int crystalPrice = 10;
-            Console.Write("How many coins you have? ");
-            coins = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Price for 1 crystal = 10 coins. How many crystals do you want to buy? ");
-            crystalsCount = Convert.ToInt32(Console.ReadLine());
-            coins -= crystalPrice * crystalsCount;
-            Console.WriteLine($"You have {crystalsCount} crystals and {coins} coins");
+            int peopleInLine;
+            int appointmentTime = 10;
+            int time;
+            int minutes;
+            int hours;
+            Console.Write("How many people are in line? ");
+            peopleInLine = Convert.ToInt32(Console.ReadLine());
+            time = peopleInLine * appointmentTime;
+            hours = time / 60;
+            minutes = time % 60;
+            Console.WriteLine($"You have to wait for your turn for {hours} hours and {minutes} minutes");
         }
     }
 }
