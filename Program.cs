c9e8479ab809744dae380a88a50871b8d72db026
KupavaN <pavelkuri@yandex.ru>
2022-05-14 12:04:51 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,19 +9,23 @@ namespace ConsoleApp1
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {            
+            int mainMenuActive = 1; // 1 = true
             string userInput;
-            int iterationCount;
 
-            Console.Write("Enter your messege ");
-            userInput = Console.ReadLine();
-            Console.Write("How many times to repeat the message ");
-            iterationCount = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 0;i < iterationCount;i++)
+            while (mainMenuActive > 0)
             {
-                Console.WriteLine(userInput);                
+                Console.WriteLine("You are in the main menu");
+                Console.WriteLine("Enter Exit to close programm");
+                userInput = Console.ReadLine();
+                if (userInput == "Exit")
+                {
+                    Console.WriteLine("Exit protocol activate :) ");
+                    break;
+                }
             }
+
+
         }
     }
 }
