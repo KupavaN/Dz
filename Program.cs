97e75ae4082b7105b97174d486fbbb70b19d4f8e
KupavaN <pavelkuri@yandex.ru>
2022-05-20 15:34:31 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,16 +9,45 @@ namespace ConsoleApp1
     internal class Program
     {
         static void Main(string[] args)
-        {                     
-            int firstNumber = 5;
-            int loopScore = 7;
-            int finalScore = 96;
+        {           
+           
+           Random rand = new Random();
+            int number = rand.Next(0, 101);
+            int firstNumberMultiple = 3;
+            int secondNumberMultiple = 5;
+            int multipleOne = firstNumberMultiple;
+            int multipleTwo = secondNumberMultiple;                       
+            bool isNumberDivideToFirstNumberMultiple = firstNumberMultiple < number;
+            bool isNumberDivideToSecondNumberMultiple = secondNumberMultiple < number;
+            bool isTheSumCorrect = isNumberDivideToFirstNumberMultiple == true && isNumberDivideToSecondNumberMultiple == true;
+            
+            while (isTheSumCorrect == true)
+            {                
+                if (isNumberDivideToFirstNumberMultiple == true)
+                {
+                    Console.WriteLine(firstNumberMultiple);
+                    firstNumberMultiple += multipleOne;                    
+                    if (secondNumberMultiple < firstNumberMultiple)
+                    {
+                        isNumberDivideToSecondNumberMultiple = true;
+                    }
+                    if (firstNumberMultiple > number)
+                    {
+                        isNumberDivideToFirstNumberMultiple = false;
+                    }                                       
+                    if (secondNumberMultiple > number || secondNumberMultiple > firstNumberMultiple)
+                    {
+                        isNumberDivideToSecondNumberMultiple = false;
+                    }                    
+                        if (isNumberDivideToSecondNumberMultiple == true)
+                    {
+                        Console.WriteLine(secondNumberMultiple);
+                        secondNumberMultiple += multipleTwo;
+                    }                                            
+                }
 
-
-            for (int score = firstNumber; score <= finalScore; score += loopScore)
-            {
-                Console.WriteLine(score); 
-            }                        
-        } 
+            }                                            
+        }  
     }
 }
+
