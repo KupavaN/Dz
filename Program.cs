9ee12f2b9d1f344449cb496105ef2ff80ca6bdda
KupavaN <pavelkuri@yandex.ru>
2022-06-05 14:28:41 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,26 +10,28 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string userInput = "";
-            string userSign;
-            int frameLength = 0;
-            int firstAndLastFrameSign = 2;
-            string frameString = "";
+            string password = "123";
+            int attempts = 4;
+            string userInput;
 
-            Console.WriteLine("Enter your word");
-            userInput = Console.ReadLine();
-            Console.WriteLine("Enter the symbol from which the rectangle will be created. For eximple F");
-            userSign =Console.ReadLine();
-            frameLength = userInput.Length+firstAndLastFrameSign;
-                        
-            for (int i = 0; i < (frameLength); i++)
-            {
-                frameString = frameString + userSign;
+            while (attempts-- > 0)
+            {                
+                Console.WriteLine($"You have {attempts} attempts");
+                Console.WriteLine("Enter password");                              
+                userInput = Console.ReadLine();
+                if (userInput == password)
+                {
+                    Console.WriteLine("Secret messege: \nHello world!");                    
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid password");
+                    
+                }                
+                Console.Clear();                                
             }
 
-            Console.WriteLine(frameString);
-            Console.WriteLine(userSign+userInput+userSign);
-            Console.WriteLine(frameString);
         }
     }
 }
