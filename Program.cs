24a753c7d780c0e703d1f6a068b00c9e58d06e5e
KupavaN <pavelkuri@yandex.ru>
2022-07-20 11:24:34 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,35 +10,56 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int[,] array = { { 1, 2, 3 }, { 7, 5, 4 }, { 6, 5, 8 } };
-            int line = 2;
-            int column = 1;
-            int sum = 0;
-            int multiply = array[0, 0];
+            int[,] array = new int[10, 10];
+            Random rand = new Random();
+            int maxElement = int.MinValue;            
+            int matrixZero = 0;
 
             for (int i = 0; i < array.GetLength(0); i++)
             {
 
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
+                    array[i, j] = rand.Next(1, 10);
                     Console.Write($"{array[i, j]} | ");
                 }
 
                 Console.WriteLine();
             }
 
-            for (int j = 0; j < array.GetLength(1); j++)
+            Console.WriteLine("\n***************************\n***************************\n***************************\n");
+
+            for (int i = 0; i < array.GetLength(0); i++)
             {
-                sum = sum + array[line, j];
+
+                for (int j = 0; j < array.GetLength(1); j++)
+                {  
+                    
+                    if (maxElement < array[i,j])
+                    {
+                        maxElement = array[i,j];                        
+                    } 
+                    
+                }                                
             }
 
             for (int i = 0; i < array.GetLength(0); i++)
             {
-                multiply = multiply * array[i, column];
-            }
 
-            Console.WriteLine($"sum = {sum}");
-            Console.WriteLine($"mult = {multiply}");
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+
+                    if (array[i, j] == maxElement)
+                    {
+                        array[i, j] = matrixZero;
+                    }
+
+                    Console.Write($"{array[i, j]} | ");
+                }
+
+                Console.WriteLine();
+            }
+            Console.WriteLine(maxElement);
         }
     }
 }
