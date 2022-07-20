eff3c3ea9c843e0312d44deb7b080ba8414b472d
KupavaN <pavelkuri@yandex.ru>
2022-07-20 11:43:01 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,52 +11,37 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int[,] array = new int[10, 10];
-            Random rand = new Random();
-            int maxElement = int.MinValue;            
+            Random random = new Random();
+            int maxElement = int.MinValue;
             int matrixZero = 0;
+            int minRandomNumber = 1;
+            int maxRandomNumber = 10;
 
             for (int i = 0; i < array.GetLength(0); i++)
             {
-
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    array[i, j] = rand.Next(1, 10);
+                    array[i, j] = random.Next(minRandomNumber, maxRandomNumber);
                     Console.Write($"{array[i, j]} | ");
+                    if (maxElement < array[i, j])
+                    {
+                        maxElement = array[i, j];
+                    }
                 }
-
                 Console.WriteLine();
             }
-
             Console.WriteLine("\n***************************\n***************************\n***************************\n");
 
             for (int i = 0; i < array.GetLength(0); i++)
             {
-
-                for (int j = 0; j < array.GetLength(1); j++)
-                {  
-                    
-                    if (maxElement < array[i,j])
-                    {
-                        maxElement = array[i,j];                        
-                    } 
-                    
-                }                                
-            }
-
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
-
                     if (array[i, j] == maxElement)
                     {
                         array[i, j] = matrixZero;
                     }
-
                     Console.Write($"{array[i, j]} | ");
                 }
-
                 Console.WriteLine();
             }
             Console.WriteLine(maxElement);
