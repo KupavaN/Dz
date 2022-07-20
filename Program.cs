871ecb5e23f3957bd151ef66a0b4b4a0f7fa47dc
KupavaN <pavelkuri@yandex.ru>
2022-07-20 11:53:45 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -19,31 +19,40 @@ namespace ConsoleApp1
 
             for (int i = 0; i < array.GetLength(0); i++)
             {
+
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
                     array[i, j] = random.Next(minRandomNumber, maxRandomNumber);
                     Console.Write($"{array[i, j]} | ");
+                   
                     if (maxElement < array[i, j])
                     {
                         maxElement = array[i, j];
                     }
                 }
+
                 Console.WriteLine();
             }
+
             Console.WriteLine("\n***************************\n***************************\n***************************\n");
 
             for (int i = 0; i < array.GetLength(0); i++)
             {
+
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
+
                     if (array[i, j] == maxElement)
                     {
                         array[i, j] = matrixZero;
                     }
+
                     Console.Write($"{array[i, j]} | ");
                 }
+
                 Console.WriteLine();
             }
+
             Console.WriteLine(maxElement);
         }
     }
