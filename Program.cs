171d7ed95aebd460a89674782384a2de80694181
KupavaN <pavelkuri@yandex.ru>
2022-07-21 16:55:07 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,45 +9,37 @@ namespace ConsoleApp1
     internal class Program
     {
         static void Main(string[] args)
-        {
-            int arrayLength = 0;
-            int[] array = new int[arrayLength];           
-            int userNumber;
-            bool isActive = true;
-            int[] arraySum = new int[array.Length + 1];
-            int sum = 0;
-            int sumBase = 0;
+        {            
+            int[] array = new int[0];
+            int[] arraySum = new int[array.Length];
+            bool isActive = true;                        
 
             while (isActive)
             {
                 Console.WriteLine("Enter the number you want add to the array:");
                 Console.WriteLine("Enter 'sum' to sum all array numbers: ");
                 Console.WriteLine("enter 'exit' if you want close the programm: \n");
-                string userInput = Console.ReadLine();
+                string userInput = Console.ReadLine();                
 
                 if (userInput != "sum" && userInput != "exit")
-                {
-                    userNumber = Convert.ToInt32(userInput);
-                    arrayLength++;
-                    array = new int[arrayLength];
+                {                    
+                    int userNumber = Convert.ToInt32(userInput);                    
+                    array = new int[array.Length +1];
 
-                    for (int i = 0; i < arraySum.Length; i++)
+                    for (int i =0; i < arraySum.Length; i++)
                     {
                         array[i] = arraySum[i];
                     }
 
-                    array[arrayLength - 1] = userNumber;
-                    arraySum = new int[arrayLength];
-
-                    for (int i = 0; i < array.Length; i++)
-                    {
-                        arraySum[i] = array[i];
-                    }
-
-                    arraySum[arrayLength - 1] = userNumber;                    
+                    array[array.Length-1] =userNumber;
+                    arraySum = array;
+                    arraySum = new int[arraySum.Length + 1];
+                    arraySum = array;
                 }
                 else if (userInput == "sum")
                 {
+                    int sum = 0;
+                    int sumBase = 0;
 
                     for (int i = 0; i < array.Length; i++)
                     {
