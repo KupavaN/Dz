2f4997a7a943e389c9f17c32228dcaaaddd36c38
KupavaN <pavelkuri@yandex.ru>
2022-07-21 18:48:36 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,47 +10,38 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {            
-            int[] array = new int[0];
-            int[] arraySum = new int[array.Length];
-            bool isActive = true;                        
-
+            int[] array = new int[0];            
+            bool isActive = true;
+            
             while (isActive)
             {
                 Console.WriteLine("Enter the number you want add to the array:");
                 Console.WriteLine("Enter 'sum' to sum all array numbers: ");
                 Console.WriteLine("enter 'exit' if you want close the programm: \n");
-                string userInput = Console.ReadLine();                
+                string userInput = Console.ReadLine();
 
                 if (userInput != "sum" && userInput != "exit")
                 {
-                    int arrayChecker = 1;
-                    int userNumber = Convert.ToInt32(userInput);                    
-                    array = new int[array.Length + arrayChecker];
-
-                    for (int i =0; i < arraySum.Length; i++)
+                    int userNumber = Convert.ToInt32(userInput);
+                    int[] tempArray = new int[array.Length + 1];
+                    for (int i = 0; i < array.Length; i++)
                     {
-                        array[i] = arraySum[i];
-                    }
-
-                    array[array.Length- arrayChecker] =userNumber;
-                    arraySum = array;
-                    arraySum = new int[arraySum.Length + arrayChecker];
-                    arraySum = array;
+                        tempArray[i] = array[i];
+                    }                   
+                    tempArray[tempArray.Length - 1] = userNumber;
+                    array = tempArray;
                 }
                 else if (userInput == "sum")
                 {
                     int sum = 0;
-                    int sumBase = 0;
 
                     for (int i = 0; i < array.Length; i++)
                     {
                         sum += array[i];
                     }
 
-                    Console.WriteLine($"Sum = {sum}");
-                    sum = sumBase;
-                    
                     Console.WriteLine();
+                    Console.WriteLine($"Sum = {sum}");
                 }
                 else if (userInput == "exit")
                 {
