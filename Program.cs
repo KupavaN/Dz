d546ee2f524c2faf3edb95eee17b359a69a9c131
KupavaN <pavelkuri@yandex.ru>
2022-07-22 12:15:04 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,42 +10,43 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int[] array = new int[30];
-            Random random = new Random();
-            int randomMin = 0;
-            int randomMax = 10;            
+            int[] array = new int[30] { 1, 2, 3, 1, 0, 1, 7, 8, 5, 4, 4, 4, 5, 5, 5, 5, 5, 5, 9, 2, 1, 3, 5, 7, 8, 9, 1, 0, 0, 6 };
             int numberRepittingChecker = 1;
-            int numberRepittingCheckerBase = 1;
-            int maxRepeatNumber = 0;
-            int maxRepittingOfNumber = 0;
-            int firstCheckNumber = 1;
+            int repeatNumber = 0;
+            int maxRepittingOfNumber = 1;
+
 
 
             for (int i = 0; i < array.Length; i++)
             {
-                array[i] = random.Next(randomMin, randomMax);
                 Console.Write($"{array[i]} ");
             }
 
             Console.WriteLine();
 
-            for (int i = firstCheckNumber; i < array.Length; i++)
+            for (int i = 0; i < array.Length - 1; i++)
             {
-                if (array[i] == array[i - 1])
+
+                if (array[i] == array[i + 1])
                 {
                     numberRepittingChecker++;
+
+                    if (numberRepittingChecker > maxRepittingOfNumber)
+                    {
+                        maxRepittingOfNumber = numberRepittingChecker;
+                        repeatNumber = array[i];
+                    }
+
                 }
-                else if (maxRepittingOfNumber < numberRepittingChecker)
+                else
                 {
-                    maxRepittingOfNumber = numberRepittingChecker;
-                    maxRepeatNumber = array[i - 1];
-                    numberRepittingChecker = numberRepittingCheckerBase;
+                    numberRepittingChecker = 1;
                 }
-                
+
             }
 
             Console.WriteLine();
-            Console.WriteLine($"Max repeat Number = {maxRepeatNumber}. Max repeat Number count = {maxRepittingOfNumber} . ");
+            Console.WriteLine($"Max repeat Number = {repeatNumber}. Max repeat Number count = {maxRepittingOfNumber} . ");
 
         }
     }
