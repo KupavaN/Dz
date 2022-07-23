6470c1dce855820419ead059d87589d843e9822a
KupavaN <pavelkuri@yandex.ru>
2022-07-23 13:52:50 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,40 +10,40 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int[] array = new int[30] { 1, 2, 3, 1, 0, 1, 7, 8, 5, 4, 4, 4, 5, 5, 5, 5, 5, 5, 9, 2, 1, 3, 5, 7, 8, 9, 1, 0, 0, 6 };
-            int numberRepittingChecker = 1;
-            int repeatNumber = 0;
-            int maxRepittingOfNumber = 1;
-            int numberRepittingCheckerBase = 1;
+            int[] array = { 3, 5, 7, 4, 1, 0,9,2,4,8 };
+            int arrayLength = array.Length;
+            int numberCheck = 0;
 
-            for (int i = 0; i < array.Length; i++)
+            for(int i = 0; i < arrayLength; i++)
             {
                 Console.Write($"{array[i]} ");
             }
 
             Console.WriteLine();
 
-            for (int i = 0; i < array.Length - 1; i++)
+            for(int i = arrayLength-1; i > 0; i--)
             {
 
-                if (array[i] == array[i + 1])
+                for (int j =0; j<i; j++)
                 {
-                    numberRepittingChecker++;
 
-                    if (numberRepittingChecker > maxRepittingOfNumber)
+                    if (array[j] > array[j + 1])
                     {
-                        maxRepittingOfNumber = numberRepittingChecker;
-                        repeatNumber = array[i];
+                        numberCheck=array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = numberCheck;
                     }
-                }
-                else
-                {
-                    numberRepittingChecker = numberRepittingCheckerBase;
                 }
             }
 
             Console.WriteLine();
-            Console.WriteLine($"Max repeat Number = {repeatNumber}. Max repeat Number count = {maxRepittingOfNumber} . ");
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+
+            Console.WriteLine();
         }
     }
 }
