122132f705dd73757732d7552607886bd67e2432
KupavaN <pavelkuri@yandex.ru>
2022-08-20 20:50:21 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,29 +12,36 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int[] array = { 2,4,8,9 };
-            Random random = new Random();
-
-            CountArray(array);
-           
-            for (int i = array.Length - 1; i >= 1; i--)
-            {
-                int randomIndex = random.Next(i + 1);
-                int shuffleIndex = array[randomIndex];
-                array[randomIndex] = array[i];
-                array[i] = shuffleIndex;                
-            }
-
-            Console.WriteLine();
-
+                                            
             CountArray(array);
         }
 
         static void CountArray(int[] array)
         {
+            Random random = new Random();
+
             for (int j = 0; j < array.Length; j++)
             {
                 Console.Write(array[j]);
             }
+
+            for (int i = array.Length - 1; i >= 1; i--)
+            {
+                int randomIndex = random.Next(i + 1);
+                int shuffleIndex = array[randomIndex];
+                array[randomIndex] = array[i];
+                array[i] = shuffleIndex;
+            }
+
+            Console.WriteLine();
+
+            for (int j = 0; j < array.Length; j++)
+            {
+                Console.Write(array[j]);
+            }
+
+            Console.WriteLine();
+
         }
     }
 }
