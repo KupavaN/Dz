3c7791468a6ecb3a7a8341bcfcb141c31bcc0605
KupavaN <pavelkuri@yandex.ru>
2022-08-20 17:12:31 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -10,27 +11,171 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(ReadInt());
-        }
+            Console.CursorVisible = false;
+            bool isPlaying = true;
+            bool isGameMenuActive = true;
+            int pacmanX;
+            int pacmanY;
+            int pacmanDX = 0;
+            int pacmanDY = 1;
+            string mapName = "map1";
 
-        static int ReadInt()
-        {
-            int convert = 0;
-            bool isConversionDone = false;
-
-            while (isConversionDone == false)
+            while (isGameMenuActive)
             {
-                Console.Write("Enter number: ");
-                string input = Console.ReadLine();
-                isConversionDone = int.TryParse(input, out convert);
+                Console.WriteLine("Choose mode: \n 1 - play on the created map. \n 2 - create your own map. \n 3 - close game. \n\n Attention!!! If you want to log out during the game press Esc!!!");
+                string userInput = Console.ReadLine();
+                Console.Clear();
 
-                if (isConversionDone == false)
-                {                    
-                    Console.WriteLine("The conversion failed. Try again");
+                if (userInput == "1")
+                {
+                    mapName = "map1";
+                    isGameMenuActive = false;
+                }
+                else if (userInput == "2")
+                {
+                    MapCreation(mapName);
+                    mapName = "map2";
+                    isGameMenuActive = false;
+                    Console.CursorVisible = false;
+
+                }
+                else if (userInput == "3")
+                {
+                    isGameMenuActive = false;
+                    isPlaying = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
                 }
             }
-            return convert;
+            char[,] map = ReadMap(mapName, out pacmanX, out pacmanY);
+            DrawMap(map);
+
+            while (isPlaying)
+            {
+                Console.SetCursorPosition(pacmanY, pacmanX);
+                Console.Write('@');
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    ChangeDirection(key, ref pacmanDX, ref pacmanDY, ref isPlaying);
+
+                }
+
+                if (map[pacmanX + pacmanDX, pacmanY + pacmanDY] != '#')
+                {
+                    Move(ref pacmanX, ref pacmanY, pacmanDX, pacmanDY);
+
+                }
+                System.Threading.Thread.Sleep(160);
+
+            }
         }
 
+        static void ChangeDirection(ConsoleKeyInfo key, ref int DX, ref int DY, ref bool isPlaying)
+        {
+            isPlaying = true;
+            switch (key.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    DX = -1; DY = 0;
+                    break;
+                case ConsoleKey.DownArrow:
+                    DX = 1; DY = 0;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    DX = 0; DY = -1;
+                    break;
+                case ConsoleKey.RightArrow:
+                    DX = 0; DY = 1;
+                    break;
+                case ConsoleKey.Escape:
+                    isPlaying = false;
+                    Console.Clear();
+                    break;
+            }
+
+        }
+
+        static void Move(ref int X, ref int Y, int DX, int DY)
+        {
+            Console.SetCursorPosition(Y, X);
+            Console.WriteLine(" ");
+
+            X += DX;
+            Y += DY;
+
+            Console.SetCursorPosition(Y, X);
+            Console.Write('@');
+
+        }
+
+        static void DrawMap(char[,] map)
+        {
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    Console.Write(map[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static char[,] ReadMap(string mapName, out int pacmanX, out int pacmanY)
+        {
+            pacmanX = 0;
+            pacmanY = 0;
+            string[] newFile = File.ReadAllLines($"maps/{mapName}.txt");
+            char[,] map = new char[newFile.Length, newFile[0].Length];
+
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    map[i, j] = newFile[i][j];
+
+                    if (map[i, j] == '@')
+                    {
+                        pacmanX = i;
+                        pacmanY = j;
+                    }
+                }
+            }
+            return map;
+        }
+
+        static string MapCreation(string mapName)
+        { 
+            mapName =("map2");
+            bool isCreating = true;
+            Console.CursorVisible = true;
+            Console.WriteLine("Now you are available to create your own map. \n Attention!!! The borders of the map must be equal");
+            Console.WriteLine("Example:\n Correct:\n\n ##### \n #   # \n # # # \n #   # \n #  ## \n #####");
+            Console.WriteLine("Incorrect:\n\n ##### \n #   ## \n # # ### \n #  ## \n #####");
+            Console.WriteLine("Now you can create your map. \n Dont forget to create player sign like this @");
+            StreamWriter sw = new StreamWriter($"maps/{mapName}.txt");
+            Console.WriteLine("Enter 1 to create a new line on the map. \nEnter 2 to complete creation map.");
+
+            while (isCreating)
+            {            
+                string input = Console.ReadLine();
+
+                if (input == "1")
+                {
+                    string userInput = Console.ReadLine();
+                    sw.WriteLine(userInput);                    
+                }
+                else if (input == "2")
+                {
+                    sw.Close();
+                    isCreating = false;
+                    Console.Clear();
+                }                
+            }            
+            return mapName;            
+        }
     }
 }
