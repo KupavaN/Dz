be86d4cc6424574b6d80b9b888b6827d5d276acf
KupavaN <pavelkuri@yandex.ru>
2022-08-26 18:05:13 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,23 +11,55 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int[] array = { 2,4,8,9 };
-                                            
-            MixArray(array);
-        }
-
-        static void MixArray(int[] array)
-        {
-            Random random = new Random();
+            List<string> words = new List<string>();
+            List<string> wordsMeaning = new List<string>();
+            words.Add("Watermelon");
+            words.Add("Apple");
+            words.Add("Pie");
+            words.Add("Gold");
+            wordsMeaning.Add("A large, round, green fruit that is pink inside with a lot of black seeds");
+            wordsMeaning.Add("A hard, round fruit with a green or red skin");
+            wordsMeaning.Add("A type of food made with meat, vegetables, or fruit which is covered in pastry and baked");
+            wordsMeaning.Add("A valuable, shiny, yellow metal used to make coins and jewellery (symbol Au)");
+            bool isDictionaryOpen = true;
             
-            for (int i = array.Length - 1; i >= 1; i--)
+            while (isDictionaryOpen)
             {
-                int randomIndex = random.Next(i + 1);
-                int shuffleIndex = array[randomIndex];
-                array[randomIndex] = array[i];
-                array[i] = shuffleIndex;
-            }            
+                Console.WriteLine("There are 4 words in our dictionary : Waterlemon, Apple, Pie, Gold.");
+                Console.WriteLine("1 - if you want enter the wold. \n2 - Close the dictionary ");
+                int chooseStep = Convert.ToInt32(Console.ReadLine());
 
+                if (chooseStep == 1)
+                {
+                    Console.WriteLine("Enter the word:");
+                    string userInput = Console.ReadLine();
+
+                    for (int i = 0; i < words.Count; i++)
+                    {
+                        if (userInput == words[i])
+                        {
+                            Console.WriteLine(wordsMeaning[i]);
+                            Console.Read();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Unknown word.");
+                            Console.Read();                            
+                        }
+                    }
+                }
+                else if (chooseStep == 2)
+                {
+                    isDictionaryOpen = false;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect input.");
+                    Console.Read();
+                }
+                Console.Clear();
+                Console.Read();
+            }            
         }
     }
 }
