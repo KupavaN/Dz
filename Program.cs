6c9b8a4323d091ed6f4a937b576f340e3c53cf88
KupavaN <pavelkuri@yandex.ru>
2022-09-06 12:31:21 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,48 +11,26 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Dictionary<int, string> dictionary = new Dictionary<int, string>()
+            Queue <int> buyers = new Queue<int>();
+            buyers.Enqueue(100);
+            buyers.Enqueue(75);
+            buyers.Enqueue(380);
+            buyers.Enqueue(97);
+            buyers.Enqueue(34);
+            buyers.Enqueue(421);
+            int till = 0;
+            
+            while (buyers.Count > 0)
             {
-                {1,"A large, round, green fruit that is pink inside with a lot of black seeds" },
-                {2, "A hard, round fruit with a green or red skin" },
-                {3, "A type of food made with meat, vegetables, or fruit which is covered in pastry and baked" },
-                {4, "A valuable, shiny, yellow metal used to make coins and jewellery (symbol Au)" }
-            };
-            const string Watermelon = "1";
-            const string Apple = "2";
-            const string Pie = "3";
-            const string Gold = "4";
-
-            bool isDictionaryOpen = true;
-
-            while (isDictionaryOpen)
-            {
-                Console.WriteLine($"There are 4 words in our dictionary :Watermelon = {Watermelon},Apple = {Apple},Pie = {Pie},Gold = {Gold}.");
-                Console.WriteLine("Enter the number of the word you want to know the meaning of. \n0 - Close the dictionary ");
-                int userInput = Convert.ToInt32(Console.ReadLine());
-
-                if (dictionary.ContainsKey(userInput))
-                {
-                    Console.WriteLine(dictionary[userInput]);
-                    Console.Read();
-                }
-                else if (userInput !=0 )
-                {
-                    Console.WriteLine("Unknown word.");
-                    Console.Read();
-                }
-
-                if (userInput == 0)
-                {
-                    isDictionaryOpen = false;
-                }
-                else
-                {
-                    Console.WriteLine("Incorrect input.");
-                    Console.Read();
-                }
-                Console.Clear();                
+                Console.WriteLine($"Cash in till: {till}. Press any button to serve the buyer\n");
+                Console.ReadKey();
+                Console.Clear();
+                till += buyers.Dequeue();                
             }
+
+            Console.WriteLine($"Cash in till: {till}. All buyers served. Press any button to close the programm. \n");
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 }
