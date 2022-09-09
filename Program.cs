227901a45b3ba8ad1b8eed5fc91b5b0d6fd11120
KupavaN <pavelkuri@yandex.ru>
2022-09-09 21:23:43 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,87 +11,44 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            const string NewDossier = "1";
-            const string ShowAllDossier = "2";
-            const string DossierToRemove = "3";
-            const string Exit = "4";
-            Dictionary<string, string> staff = new Dictionary<string, string>();
-            staff.Add("Petrov Ivan Sidorovich", "engeneer");
-            staff.Add("Ivanov Sidor Petrovich", "driver");
-            staff.Add("Sidorov Petr Ivanovich", "artist");
-            bool isWork = true;
+            int[] array = new int[] { 4, 3, 2 ,1};
+            int[] array2 = new int[] { 2, 3, 5};
+            int[] array3 = new int[] { };
+            List<int> list = new List<int>();
 
-            while (isWork)
+            if (array.Length > array2.Length)
             {
-                Console.WriteLine($"Add dossier - {NewDossier} \n" +
-                    $"Show dossier - {ShowAllDossier} \n" +
-                    $"Remove dossier -{DossierToRemove} \n" +
-                    $"Exit programm -{Exit}");
-                string userInput = Console.ReadLine();
+                array3 = array;
+                Converter(array2, array, array3);
+            }
+            else
+            {
+                array3 = array2;
+                Converter(array, array2, array3);
+            }
 
-                switch (userInput)
+            for (int i = 0; i < array3.Length; i++)
+            {
+                list.Add(array3[i]);
+            }
+
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void Converter(int[] array, int[] array2, int[] array3)
+        {
+            for (int j = 0; j < array.Length; j++)
+            {
+                if (array[j] > array3[j])
                 {
-                    case NewDossier:
-                        AddDossier(staff);
-                        break;
-                    case ShowAllDossier:
-                        ShowDossier(staff);
-                        break;
-                    case DossierToRemove:
-                        RemoveDossier(staff);
-                        break;
-                    case Exit:
-                        isWork = false;
-                        break;
-                    default:
-                        Console.WriteLine("Incorrect input.");
-                        break;
+                    array3[j] = array[j];
                 }
-
-                Console.ReadKey();
-                Console.Clear();
-            }
-        }
-
-        static void AddDossier(Dictionary<string, string> staff)
-        {
-            Console.WriteLine("Enter new employee data");
-            string newWorkerData = Console.ReadLine();
-            Console.WriteLine("Enter new employee position");
-            string newWorkerposition = Console.ReadLine();
-
-            if (staff.ContainsKey(newWorkerData) == false)
-            {
-                staff.Add(newWorkerData, newWorkerposition);
-            }
-            else
-            {
-                Console.WriteLine("This data is already in the database");
-            }
-        }
-
-        static void ShowDossier(Dictionary<string, string> staff)
-        {
-            foreach (var person in staff)
-            {
-                Console.WriteLine($"{person.Key} - {person.Value}");
-            }
-        }
-
-        static void RemoveDossier(Dictionary<string, string> staff)
-        {
-            Console.WriteLine("Enter employee data to delete it");
-            string deleteWorkerData = Console.ReadLine();
-
-            if (staff.ContainsKey(deleteWorkerData) == false)
-            {
-                Console.WriteLine("No employee found.");
-            }
-            else
-            {
-                staff.Remove(deleteWorkerData);                
             }
         }
     }
 }
+
 
